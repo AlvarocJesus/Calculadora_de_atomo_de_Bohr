fd441a1b79e86a00a71fcdb40cf9ed8b322f931f
AlvarocJesus <alvim.coelho.jesus@gmail.com>
2022-09-12 11:52:51 -0300

    Adiciona menu e funcao inical do projeto

--- main.py
+++ main.py
@@ -1,35 +1,54 @@
 from time import sleep
 
-
+# Exibe integrantes dos grupos
 print("""
 ----------INTEGRANTES----------
 Alvaro Coelho Jesus 22221002-3
 """)
 
+# Exibe descriçao do projeto
+print('')
+
 def message():
   input("""
-  ------------------------------
-  Digite ENTER para continuar com os cálculos
-  ------------------------------
-  """)
+------------------------------
+Digite ENTER para continuar com os cálculos
+------------------------------
+""")
+
+def quanticNumber(n):
+  print(f'Numero quantico: {n}')
 
 while True:
-  print()
+  print("\n-----------------Menu-----------------")
+  print("""Indique a sua entrada:
+1 - Numero quântico
+2 - Numero quântico inicial e numero quântico final
+3 - Numero quântico e frequência ou comprimento de onda do fóton absorvido
+4 - Numero quântico e frequência ou comprimento de onda do fóton emitido
+
+0 - Sair
+  """)
   option = int(input())
 
   if option == 0:
-    sleep(1000)
-    print('Ate a proxima... (^.^)')
-    sleep(1000)
+    sleep(1)
+    print('Ate a proxima... (^.^) 👉️👈️')
+    sleep(1)
+    break
   elif option == 1:
-    print('A')
+    n = int(input('Digite o valor do numero quântico: '))
+    quanticNumber(n)
     message()
   elif option == 2:
-    print('B')
+    n = int(input('Digite o valor do numero quântico: '))
+    quanticNumber(n)
     message()
   elif option == 3:
-    print('C')
+    n = int(input('Digite o valor do numero quântico: '))
+    quanticNumber(n)
     message()
   elif option == 4:
-    print('D')
+    n = int(input('Digite o valor do numero quântico: '))
+    quanticNumber(n)
     message()
